d746cdac00f0dae09211b9b10542533c0bc766dd
Lukas Johansson <lukuxdev@gmail.com>
2021-10-07 23:19:52 +0200

    - Setup Project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import engine.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+        System.out.println("Hello world");
+    }
+}
